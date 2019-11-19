6bb193383ecd114e25da6aa8919815554c855545
MrRainx <diegocondo10@yahoo.es>
2019-11-19 09:50:05 -0500

    trabajando en los hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {
-  Component,
-  //useState
+  //Component,
+  useState, useEffect
 } from 'react';
 import './App.css';
-
+/*
 class App extends Component {
 
   constructor(props) {
@@ -12,9 +12,7 @@ class App extends Component {
       num: 0,
       user: {}
     }
-
     this.buscar = this.buscar.bind(this);
-
   }
 
   async buscar() {
@@ -32,11 +30,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <h5>{!this.state.user.title ? 'NO HA HECHO NINGUNA PETICION' : this.state.user.title}</h5>
-
         <p>Ha hecho {this.state.num} peticiones</p>
-
         <button class="btn" onClick={this.buscar}>Aumentar</button>
 
       </div>
@@ -45,39 +40,45 @@ class App extends Component {
   }
 
 }
+*/
 
-/*
+
 const App = () => {
 
   const [user, setUser] = useState({});
   const [num, setNum] = useState(0);
 
+
   const buscar = async () => {
-
-    const json = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-
+    const json = await fetch(`https://jsonplaceholder.typicode.com/todos/${num + 1}`)
     const user = await json.json().catch(err => console.log)
-
     setUser(user)
     setNum(num + 1)
+
   }
 
 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${num + 1}`)
+    .then(json => json.json())
+    .then(data => console.log)
+
+    return () => {
+      document.title = 'TERMINADO'
+    }
+
+  }, [num]);
+
   return (
     <div className="App">
-
       <h5>{!user.title ? 'NO HA HECHO NINGUNA PETICION' : user.title}</h5>
-
       <p>Ha hecho {num} peticiones</p>
-
       <button class="btn" onClick={buscar}>
         Aumentar
     </button>
-
     </div>
   );
 
 }
-*/
 
 export default App;
